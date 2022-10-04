b459d290fb52eb5175cadf57360455ceb37354c1
roman liburkin <rpmanliburkin@gmail.com>
2022-10-04 22:48:05 +0300

    add instructions and unittests

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print('hello world')
-print('sorry')
+from cmath import isfinite
+import unittest
+
+class TestSimpleMath(unittest.TestCase):
+
+    def test_sum(self):
+        a = 2 + 2
+        self.assertEqual(a, 4)
+
+    def test_isfinite(self):
+        b=4
+        self.assertTrue(b,isfinite)
+
+if __name__ == '__main__':
+    unittest.main()
